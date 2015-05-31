7023a709e1910c01df50dd569df2f5b2c0f7935f
pdpalfreyman <14enigma@gmail.com>
2015-05-31 14:18:25 -0600

    updated

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,44 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj){
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam
+		var homeScore = parseInt(gameObj.homeTeamScore)
+		var oppScore = parseInt(gameObj.opponentScore)
+		if(homeScore > oppScore){
+			gameObj.won = true
+		} else {
+			gameObj.won = false
+		};
+		return $http({
+			method: 'POST',
+			url: url,
+			data: gameObj
+		});
+	};
+
+	this.getTeamData = function(team){
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data){
+			var results = data.data.results;
+			var wins = 0
+			var losses = 0
+			for(var i = 0; i < results.length; i++){
+				if(results.won[i] === true){
+					wins++
+				} else {
+					losses++
+				};
+			results.wins = wins;
+			results.losses = losses;
+			};
+		});
+
+		return deferred.promise;
+	};
+
 });
